01aa7363168af0b2fec89ef5ce6d3b8da1b21af9
Asmit Das Choudhury <asmitdaschoudhury@proton.me>
2026-08-11 12:28:01 +0530

    Time: 11 ms (24.78%), Space: 24.9 MB (6.38%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,6 +4,8 @@ class Solution:
         using HashMaps, reverse sorting the key-val pairs and putting the keys in another list
         Simple but higher time complexity: O(n*nlogn)
         '''
+
+        '''
         words = {}
         for num in nums:
             if num not in words:
@@ -18,5 +20,32 @@ class Solution:
         res = []
         for i in range(k):
             res.append(op[i][1])
+
+        return res
+        '''
+        
+        '''
+        using HashMap and Bucket Sort (storing in buckets of frequencies of occurence)
+        Doesn't requires sorting, so has a time complexity of O(n) (O(n+m+k))
+        '''
+
+        count = {}
+        buckets = [[] for _ in range(len(nums)+1)]
+
+        for num in nums:
+            if num not in count:
+                count[num] = 0
+            count[num] += 1
+        
+        for num, count in count.items():
+            buckets[count].append(num)
+
+        res = []
+        while len(res) < k:
+            val = buckets.pop()
+            if not val:
+                continue
+            for num in val:
+                res.append(num)
 
         return res
