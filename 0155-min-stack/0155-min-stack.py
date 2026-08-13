4882d455b235220a0031419e9db659a54196c351
Asmitx026 <asmitdaschoudhury@proton.me>
2026-08-14 00:50:04 +0530

    fix(typo): Time: 123 ms (14.75%), Space: 31.2 MB (73.62%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,14 +1,8 @@
 class MinStack:
     '''
-    [DISCARDED cuz too complex for a simple problem]
-    Using a min integer to track the min value plus storing encoded (difference between min and the value) values in the stack
-    Leads to simple 0(1) time complexity for the getMin() function
-    '''
-
-    '''
     Using two stacks, one to store the elements and one to store the min. numbers (not all elements in the stack tho)
-    Simpler plus O(1) time complexity but high space complexity ((O(n+m))
-    NOTE: min stack stores only the elements smaller than its predecessors
+    Simpler plus O(1) time complexity but high space complexity (O(n+m) ~ O(n))
+    NOTE: min stack stores only the elements equal to or smaller than its predecessors
     '''
 
     def __init__(self):
@@ -43,3 +37,44 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+    '''
+    [DISCARDED cuz too complex for a simple problem]
+    Using a min integer to track the min value plus storing encoded (difference between min and the value) values in the stack
+    Leads to simple 0(1) time complexity for the getMin() function
+
+    def __init__(self):
+        self.stack = []
+        self.min = float('inf')
+
+    def push(self, value: int) -> None:
+        if not self.stack:
+            self.min = value
+            self.stack.append(0)
+            return
+
+        self.stack.append(value - self.min)
+        if self.min > value:
+            self.min = value
+
+    def pop(self) -> None:
+        if not self.stack:
+            return
+
+        pop = self.stack.pop()
+        if pop < 0:
+            self.min = self.min - pop
+
+    def top(self) -> int:
+        if not self.stack:
+            return
+
+        top = self.stack[-1]
+        if top < 0:
+            return self.min
+        else:
+            return top + self.min
+
+    def getMin(self) -> int:
+        return self.min
+    '''
