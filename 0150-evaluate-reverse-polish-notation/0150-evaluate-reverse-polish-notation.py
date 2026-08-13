6b1ec2dfe9a367765b382040ebe6b3091c839f33
Asmit Das Choudhury <asmitdaschoudhury@proton.me>
2026-08-14 00:38:05 +0530

    Time: 11 ms (6.25%), Space: 22.2 MB (9.77%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,7 +1,16 @@
+class DoublyLinkedList:
+    def __init__(self, val=0, prev=None, next=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+
+
 class Solution:
     '''
     Simplest and most effective among other ones.. Leads to a time complexity of O(n)
     Also possible via Linked-List and Recursion
+    '''
+
     '''
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
@@ -22,3 +31,37 @@ class Solution:
                 stack.append(int(token))
         
         return stack[-1]
+    '''
+
+    def evalRPN(self, tokens: List[str]) -> int:
+        head = DoublyLinkedList(tokens[0])
+        curr = head
+
+        for token in tokens[1:]:
+            curr.next = DoublyLinkedList(val=token, prev=curr)
+            curr = curr.next
+        
+        ans = float('inf')
+        while head is not None:
+            if head.val in "+-*/":
+                first = int(head.prev.prev.val)
+                second = int(head.prev.val)
+                match head.val:
+                    case '+':
+                        res = first + second
+                    case '-':
+                        res = first - second
+                    case '*':
+                        res = first * second
+                    case '/':
+                        res = int(first / second) # Floor Division wont work here in case of neagtive numbers (eg. 6//-132 = -1)
+                
+                head.val = res
+                head.prev = head.prev.prev.prev
+                if head.prev is not None:
+                    head.prev.next = head
+            
+            ans = int(head.val)
+            head = head.next
+
+        return ans
