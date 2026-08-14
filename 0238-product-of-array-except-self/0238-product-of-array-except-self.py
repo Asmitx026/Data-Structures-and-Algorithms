e337d47b921a661ed38c11a2e2643b15632ac42f
Asmit Das Choudhury <asmitdaschoudhury@proton.me>
2026-08-14 19:39:17 +0530

    Time: 40 ms (11.93%), Space: 27.8 MB (13.54%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,23 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        '''
+        Prefix-Suffix Approach - O(n) time complexity but requires extra space O(N) excl. output list
+        '''
+        n = len(nums)
+        if n == 0:
+            return []
+
+        pf, sf = [0]*n, [0]*n
+        op = [0]*n
+        
+        pf[0] = sf[-1] = 1
+        for i in range(1,n):
+            pf[i] = nums[i-1] * pf[i-1]
+
+        for i in range(-2, -n-1, -1):
+            sf[i] = sf[i+1] * nums[i+1]
+
+        for i in range(n):
+            op[i] = pf[i] * sf[i]
+
+        return op
