138de07a4007630e73e15b5f9d0b7ccdb13b5720
Asmit Das Choudhury <asmitdaschoudhury@proton.me>
2026-08-15 17:02:48 +0530

    Time: 35 ms (25.64%), Space: 25.5 MB (69.83%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,7 +1,27 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         '''
+        Follow-Up Approach: 'Can you solve the problem in O(1) space complexity?'
+        Optimized Prefix-Suffix Approach - creating the prefix list, then multiplying the elements in that list (reverse loop) with the product of the suffix elements (while storing the product with each iterating in `sf`)
+        '''
+
+        n = len(nums)
+        op = [1]*n
+
+        for i in range(1,n):
+            op[i] = nums[i-1] * op[i-1]
+        
+        sf = 1
+        for i in range(2,n+1):
+            sf *= nums[-i+1]
+            op[-i] *= sf
+        
+        return op
+
+        '''
         Prefix-Suffix Approach - O(n) time complexity but requires extra space O(N) excl. output list
+        '''
+
         '''
         n = len(nums)
         pf, sf, op = [0]*n, [0]*n, [0]*n
@@ -15,3 +35,4 @@ class Solution:
             op[i] = pf[i] * sf[i]
 
         return op
+        '''
