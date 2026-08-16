d0dfba0b31524b69626f47445c5a8762f5efbf29
Asmit Das Choudhury <asmitdaschoudhury@proton.me>
2026-08-16 19:19:14 +0530

    Time: 0 ms (100%), Space: 19.4 MB (41.11%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        '''
+        Multiple other ways to solve this problem: 
+        - Extend method
+        - + & * operator
+        - Using loop and appending the values to a shallow copy (`ans`) 
+            (logical way to 'Return the array `ans`' but takes auxillary space)
+        '''
+
+        return nums+nums # most efficient way in terms of runtime and memory space
+
+        # return nums*2
+        # nums.extend(nums); return nums
+        # ans=nums.copy();for num in nums:;ans.append(num);return ans
