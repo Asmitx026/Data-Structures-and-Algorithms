583820d0d566212075eec7e8e7ab7d09be6b2d68
Asmit Das Choudhury <asmitdaschoudhury@proton.me>
2026-08-20 23:15:46 +0530

    Time: 18 ms (97.77%), Space: 19.3 MB (45.11%) - LeetHub

--- 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.py
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.py
@@ -1,5 +1,26 @@
 class Solution:
     def triangularSum(self, nums: List[int]) -> int:
+        '''
+        Solved via applying the Pascal Triangle (binomial coefficient)
+        Used the relation: C(n,k) = (C(n,k-1) × (n−k−1)) / k
+        '''
+
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+
+        c, res = 1, 0
+        for i in range(n):
+            res += nums[i] * c
+            c = (c * (n-1-i)) // (i+1)
+
+        return res % 10
+
+        '''
+        Solved via Recursion, simple but high time and space complexity, i.e., O(n²)
+        '''
+
+        '''
         n = len(nums)
         if n == 1:
             return nums[0]
@@ -11,3 +32,4 @@ class Solution:
             i += 1
 
         return self.triangularSum(newNums)
+        '''
