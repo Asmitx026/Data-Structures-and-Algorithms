78dea852ce27eaf96f377485e66c2545e7f4f1bd
Asmit Das Choudhury <asmitdaschoudhury@proton.me>
2026-08-31 01:01:23 +0530

    Time: 15 ms (8.87%), Space: 23.4 MB (12.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        '''
+        via Two Pointers
+        '''
+        
         s = ''.join(char.lower() for char in s if char.isalnum())
-        i, j = 0, len(s)-1
+        # return s == s[::-1] # even simpler approach to this problem
 
+        i, j = 0, len(s)-1
         while i < j:
             if s[i] != s[j]:
                 return False
 
             i += 1
             j -= 1
-        
         return True
