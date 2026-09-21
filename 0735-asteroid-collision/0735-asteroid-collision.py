8d5859ff344d08063ed89feedbe8e1ecd8a2821a
Asmit Das Choudhury <asmitdaschoudhury@proton.me>
2026-09-22 01:05:07 +0530

    Time: 3 ms (94.25%), Space: 20.2 MB (92.65%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,34 +1,25 @@
 class Solution:
     def asteroidCollision(self, asteroids: list[int]) -> list[int]:
+        '''
+        Simple Stack Implementation, leads to O(N) time and space complexity
+        '''
+
         stack = []
         
         for num in asteroids:
-            if len(stack) == 0:
+            if not stack:
                 stack.append(num)
                 continue
-            
-            num_positive = num > 0
-            stack_positive = stack[-1] > 0
 
-            if stack_positive and not num_positive:
+            while stack and num < 0 < stack[-1]:
                 # since collision occurs only when an asteroid (stack top) is moving towards right (positive) and the next one (num) is moving towards left (negative)
-                larger = False
-                while stack_positive != num_positive and len(stack) != 0:
-                    if abs(stack[-1]) < abs(num):
-                        stack.pop()
-                        larger = True
-                    elif abs(stack[-1]) == abs(num):
-                        stack.pop()
-                        larger = False
-                        break
-                    else:
-                        larger = False
-                        break
-
-                    stack_positive = stack[-1] > 0 if len(stack) != 0 else False
-                
-                if larger:
-                    stack.append(num)
+                if stack[-1] < -num:
+                    stack.pop()
+                elif stack[-1] == -num:
+                    stack.pop()
+                    break
+                else:
+                    break
             else:
                 stack.append(num)
 
